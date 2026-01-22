9c2325948b84b1278124cc1275c967b94567e8d4
anadruetta29 <anadruetta2023@gmail.com>
2026-01-22 01:31:04 -0300

    add: initialize server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,38 @@
-import express from "express";
+import { env } from "./config/adapters/env";
+import { AppRouter } from "./presentation/server/routes";
+import { Server } from "./presentation/server/server";
+import { Pool } from "pg";
 
-const app = express();
-const PORT = 3000;
+(async () => {
+    main();
+})();
 
-app.get("/", (req, res) => {
-  res.send("PeluGest backend running!");
-});
+async function main() {
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+	const pool = new Pool({
+        host: env.DB.HOST,
+        port: env.DB.PORT,
+        database: env.DB.NAME,
+        user: env.DB.USER,
+        password: env.DB.PASS,
+    });
+
+	try {
+        await pool.query("SELECT 1");
+        console.log("Connected to PostgreSQL successfully!");
+    } 
+	catch (error) {
+        console.error("Error connecting to PostgreSQL:", error);
+        process.exit(1);
+    }
+
+    const server = new Server({
+        port: env.PORT,
+        routes: AppRouter.routes
+    });
+
+    server.start();
+
+	console.log(`Server running on port ${env.PORT}`);
+
+}
